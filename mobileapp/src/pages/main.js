90f2dbaa9591ebee361ee47c23284d135b2124ca
Eduardo <z.eduardofaria@gmail.com>
2019-09-24 18:22:23 -0300

    Infinity scroll added

--- mobileapp/src/pages/main.js
+++ mobileapp/src/pages/main.js
@@ -9,23 +9,35 @@ export default class Main extends Component {
   };
 
   state = {
+    productInfo: {},
     docs: [],
+    page: 1,
   };
 
   componentDidMount() {
     this.loadProducts();
   }
 
-  loadProducts = async () => {
+  loadProducts = async (page = 1) => {
     try {
-      const response = await api.get('/products');
+      const response = await api.get(`/products?page=${page}`);
 
-      const {docs} = response.data;
+      const {docs, ...productInfo} = response.data;
 
-      this.setState({docs});
+      this.setState({docs: [...this.state.docs, ...docs], productInfo, page});
     } catch (error) {
       console.log(error);
     }
+  };
+
+  loadMore = () => {
+    const {page, productInfo} = this.state;
+
+    if (page === productInfo) return;
+
+    const pageNumber = page + 1;
+
+    this.loadProducts(pageNumber);
   };
 
   renderItem = ({item}) => (
@@ -47,6 +59,8 @@ export default class Main extends Component {
           data={this.state.docs}
           keyExtractor={item => item._id}
           renderItem={this.renderItem}
+          onEndReached={this.loadMore}
+          onEndReachedThreshold={0.1}
         />
       </View>
     );
